376cec79d5e566dae21f5321b759a396d398b4ee
ScuBix1 <bastian.monnin1@gmail.com>
2025-04-14 15:29:20 +0200

    add routes of last commit *

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/courses/{id}/qrcode', [CourseController::class, 'generateQR']);
+    Route::post('/attendance/scan', [AttendanceController::class, 'scan']);
+    Route::post('/attendance/sign', [AttendanceController::class, 'sign']);
+    Route::get('/admin/courses/{id}/attendances', [AdminController::class, 'attendances']);
 });
